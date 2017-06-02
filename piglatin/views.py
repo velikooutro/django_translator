b6fc386c9fc5edaba8f6dce85accdade4fbb1c1e
Raymond Mapuranga <veliko.outro@gmail.com>
2017-06-02 11:01:48 -0400

    first commit

---
+++ piglatin/views.py
@@ -0,0 +1,22 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+	return render(request,'home.html',{})
+
+def translate(request):
+	original = request.GET['originaltext'].lower()
+
+	translation = ''
+	for word in original.split():
+		if word[0] in ['a','e','i','o','u']:
+			translation += word
+			translation += 'yay '
+		else:
+			translation += word[1:]
+			translation += word[0]
+			translation += 'ay '
+			# translation += 'consonant '
+
+	return render(request, 'translate.html', {'original': original, 'translation':translation})
